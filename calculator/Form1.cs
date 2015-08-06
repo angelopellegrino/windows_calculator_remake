97360321cd44d2ee2fad30727efc82fec823ae17
angelo <angelopellegrino93@gmail.com>
2015-08-06 00:39:39 -0400

    coded in operators and the equal button. debugging needs to be done because they are not working.

--- calculator/Form1.cs
+++ calculator/Form1.cs
@@ -14,13 +14,19 @@ namespace calculator
     public enum Operator
     {
         None, Add, Minus, Divide, Multiply,
-        Sqrt, Reciprocal, Percent
+        Sqrt, Inverse, Percent
     }
     public partial class Form1 : Form
     {
 
+/* Using decimal to allow for more precision over double.
+ * May be changed later on depending on the performance 
+ * of the calculator. If it is too slow, the switch to
+ * double may be warranted and more efficient.
+ */
         private decimal total = 0;
         private decimal curVal = 0;
+        private Operator currentOperator;
 
 
         public Form1()
@@ -34,6 +40,8 @@ namespace calculator
 
         }
 
+
+// Configuring the number buttons to display text on click.
         private void btn1_Click(object sender, EventArgs e)
         {
             ShowInput(btn1.Text);
@@ -85,12 +93,117 @@ namespace calculator
             ShowInput(btn0.Text);
         }
 
+
+        private void btnDecimal_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text = txtDisplay.Text + '.';
+        }
+
+
+// Code for handling the display of numbers.
         private void ShowInput(String n)
         {
             txtDisplay.Text = txtDisplay.Text + n;
             curVal = decimal.Parse(txtDisplay.Text);
         }
 
-        
+
+// Operators.
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            applyOperator(Operator.Add);
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            applyOperator(Operator.Minus);
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            applyOperator(Operator.Divide);
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            applyOperator(Operator.Multiply);
+        }
+
+        private void btnSqrt_Click(object sender, EventArgs e)
+        {
+            applyOperator(Operator.Sqrt);
+        }
+
+        private void btnPercent_Click(object sender, EventArgs e)
+        {
+            applyOperator(Operator.Percent);
+        }
+
+        //private void btnInverse_Click(object sender, EventArgs e)
+        //{
+        //    applyOperator(Operator.Inverse);
+        //    
+        //}
+
+        //The Equals button.
+        private void btnEquals_Click(object sender, EventArgs e)
+        {
+            evaluate();
+            txtDisplay.Text = Convert.ToString(total);
+        }
+
+        //switch statement to apply the correct operrator
+        private void evaluate()
+        {
+            switch (currentOperator)
+            {
+                case Operator.Add:
+                    total += curVal;
+                    break;
+                case Operator.Minus:
+                    total -= curVal;
+                    break;
+                case Operator.Multiply:
+                    total *= curVal;
+                    break;
+                case Operator.Divide:
+                    total /= curVal;
+                    break;
+                case Operator.Sqrt:
+                    double squareRoot = System.Convert.ToDouble(curVal);
+                    double tmpTotal = Math.Sqrt(squareRoot);
+                    total = System.Convert.ToDecimal(tmpTotal);
+                    break;
+                case Operator.Percent:
+                    total = curVal / 100;
+                    break;
+                //case Operator.Inverse:
+                // toDo: code inverse.
+                case Operator.None:
+                    break;
+            }
+            curVal = 0;
+            currentOperator = Operator.None;
+        }
+
+        private void applyOperator(Operator op)
+        {
+            if (currentOperator != Operator.None)
+            {
+                evaluate();
+            }
+            else
+            {
+                total = decimal.Parse(txtDisplay.Text);
+            }
+            txtDisplay.Clear();
+            currentOperator = op;
+        }
+
+        private void ShowIput(String n)
+        {
+            txtDisplay.Text = txtDisplay.Text + n;
+            curVal = decimal.Parse(txtDisplay.Text);
+        }
     }
 }
